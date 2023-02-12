7b164dd374973959adbeb8328628c8c43b9a6db9
BPatel11 <bansaripatel@hollandandbarrett.com>
2023-02-12 01:34:39 +0000

    fix: sales, sales spilt

--- apps/client/src/pages/product-detail/ProductDetail.tsx
+++ apps/client/src/pages/product-detail/ProductDetail.tsx
@@ -1,33 +1,103 @@
 import { useEffect, useState } from "react";
-import { Product } from "sharktankpedia-schema";
+import { Product, MetaData } from "sharktankpedia-schema";
 import { loaderRef } from "../../components/Spinner";
 import { getProductDetail } from "../../utils/api/client";
 import { useParams } from "react-router-dom";
 import { convertDate } from "../../utils/util";
+import { Pie, Doughnut } from "react-chartjs-2";
 import {
-  Card,
-  CardBody,
-  CardFooter,
-  // CardTitle,
-  Row,
-  Col,
-  // Button,
-  // CardHeader,
-} from "reactstrap";
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend,
+  RadialLinearScale,
+} from "chart.js";
+import { Card, CardBody, CardFooter, Row, Col } from "reactstrap";
 import { Helmet } from "react-helmet";
 
 import "../../assets/css/paper-dashboard.css";
 
+ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  height: 500,
+  width: 500,
+};
+
 const ProductDetail = () => {
   const [product, setProduct] = useState<Product>();
   const { id } = useParams();
-  // const backgrounds = [
-  //   "bg-primary",
-  //   "bg-danger",
-  //   "bg-warning",
-  //   "bg-info",
-  //   "bg-success",
-  // ];
+  const [labels, setLabels] = useState([]);
+  const [value, setValue] = useState([]);
+  const [convertedSalesValues, setConvertedSalesValues] = useState([]);
+  const [unitEconomicsLabel, setUnitEconomicsLabel] = useState([]);
+  const [unitEconomicsValue, setUnitEconomicsValue] = useState([]);
+  const [convertedUnitEconomicsValue, setConvertedUnitEconomicsValue] =
+    useState([]);
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: `${
+          convertedSalesValues?.length > 0
+            ? (convertedSalesValues[0] as string).split(" ")[1]
+            : ""
+        }`,
+        data: convertedSalesValues.map((values: any) => values.split(" ")[0]),
+        backgroundColor: [
+          "rgba(255, 99, 132)",
+          "rgba(54, 162, 235)",
+          "rgba(255, 206, 86)",
+          "rgba(75, 192, 192)",
+          "rgba(153, 102, 255)",
+          "rgba(255, 159, 64)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const unitEconomicsData = {
+    labels: unitEconomicsLabel,
+    datasets: [
+      {
+        label: `${
+          unitEconomicsValue?.length > 0
+            ? (unitEconomicsValue[0] as string).split(" ")[1]
+            : ""
+        }`,
+        data: convertedUnitEconomicsValue,
+        backgroundColor: [
+          "rgba(255, 99, 132,0.85)",
+          "rgba(54, 162, 235,0.85)",
+          "rgba(255, 206, 86,0.85)",
+          "rgba(75, 192, 192,0.85)",
+          "rgba(153, 102, 255,0.85)",
+          "rgba(255, 159, 64,0.85)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
   useEffect(() => {
     loaderRef?.current?.show();
@@ -42,6 +112,59 @@ const ProductDetail = () => {
 
     fetchProducts();
   }, [id]);
+
+  useEffect(() => {
+    if (value) {
+      let unit = "Lakhs";
+      let conversionFactor = 1;
+
+      const valuesInCrores = value.filter((v: any) => v.includes("Crore"));
+
+      if (valuesInCrores.length >= 2) {
+        unit = "Crore";
+        conversionFactor = 0.01;
+      }
+
+      const convertedValues = value.map((v: any) => {
+        if (v.includes(unit)) {
+          return v;
+        } else {
+          let number = parseFloat(v.split(" ")[0]);
+          return (number * conversionFactor).toFixed(2) + " " + unit;
+        }
+      });
+      setConvertedSalesValues(convertedValues as any);
+    }
+  }, [value]);
+
+  useEffect(() => {
+    if (product && product?.sales) {
+      const salesLabel: any = [];
+      const salesValue: any = [];
+      product?.sales?.map((sale: any) => {
+        salesLabel.push(sale.name);
+        salesValue.push(sale.value);
+      });
+
+      setLabels(salesLabel);
+      setValue(salesValue);
+    }
+    if (product && product?.unitEconomics) {
+      const label: any = [];
+      const value: any = [];
+      const convertedValue: any = [];
+      product?.unitEconomics
+        ?.filter((_item: MetaData, index: number) => index > 0)
+        ?.map((unit: any) => {
+          label.push(unit.name);
+          value.push(unit.value);
+          convertedValue.push(unit.value.split(" ")[0]);
+        });
+      setUnitEconomicsLabel(label);
+      setUnitEconomicsValue(value);
+      setConvertedUnitEconomicsValue(convertedValue);
+    }
+  }, [product]);
 
   return (
     <>
@@ -201,28 +324,14 @@ const ProductDetail = () => {
                     </h6>
                     {product?.website ? (
                       <p className="p-card">
-                        🌎{" "}
-                        <a
-                          href={product?.website}
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          Website
-                        </a>{" "}
+                        🛒 <a href={product?.amazonLink}>Amazon</a>
                       </p>
                     ) : (
                       <></>
                     )}
                     {product?.amazonLink ? (
                       <p className="p-card">
-                        🛒{" "}
-                        <a
-                          href={product?.amazonLink}
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          Amazon
-                        </a>
+                        🛍️ <a href={product?.flipkartLink}>Flipkart</a>
                       </p>
                     ) : (
                       <></>
@@ -247,7 +356,7 @@ const ProductDetail = () => {
             </Col>
           </Row>
         </div>
-        <div className="container">
+        <div className="container mt-4">
           <Row>
             <Col md="6" className="text-center">
               <div className="card" style={{ width: "100%" }}>
@@ -256,7 +365,7 @@ const ProductDetail = () => {
                 </div>
                 <div className="card-body">
                   <h5 className="card-title" style={{ fontWeight: "bold" }}>
-                    {product?.originalAsk}
+                    {`${product?.originalAsk} Equity`}
                   </h5>
                 </div>
               </div>
@@ -294,137 +403,104 @@ const ProductDetail = () => {
             </Col>
           </Row>
         </div>
-
-        {/* <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <h1 className="text-center mb-5">Statistics</h1>
-
-              <div className="row">
-                {product?.statistics?.map((s, index) => (
-                  <div className="col-md-6 col-lg-6 mt-2">
-                    <div className="card" style={{ width: "100%" }}>
+        <div className="container mt-4">
+          <Row>
+            {product &&
+              product?.unitEconomics &&
+              product?.unitEconomics?.length > 0 && (
+                <Col md="4" className="text-center">
+                  <div className="card" style={{ width: "100%" }}>
+                    <div className={`card-header bg-ue`}>
+                      <h6 className="mt-2 ">{`Unit Economics (${product?.unitEconomics[0]?.name} : ${product?.unitEconomics[0]?.value})`}</h6>
+                    </div>
+                    <div className="card-body">
                       <div
-                        className={`card-header ${backgrounds[index]} text-white`}
+                        style={{
+                          height: "300px",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                        }}
+                        className="text-center"
                       >
-                        {s.name}
-                      </div>
-                      <div className="card-body">
-                        <h5 className="card-title">{s.value}</h5>
+                        <Doughnut data={unitEconomicsData} />;
                       </div>
                     </div>
                   </div>
-                ))}
+                </Col>
+              )}
+            {product && product?.sales && product?.sales?.length > 0 && (
+              <Col md="4" className="text-center">
+                <div className="card" style={{ width: "100%" }}>
+                  <div className={`card-header bg-info`}>
+                    <h6 className="mt-2 ">Sale and Revenue</h6>
+                  </div>
+                  <div className="card-body">
+                    <div
+                      style={{
+                        height: "300px",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                      className="text-center"
+                    >
+                      <Pie data={data} />
+                    </div>
+                  </div>
+                </div>
+              </Col>
+            )}
+
+            <Col md="4" className="text-center">
+              <div className="card" style={{ width: "100%" }}>
+                <div className={`card-header bg-ue`}>
+                  <h6 className="mt-2 ">Sales Split</h6>
+                </div>
+                <div className="card-body">
+                  <div
+                    style={{
+                      height: "300px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                    className="text-center"
+                  ></div>
+                </div>
               </div>
-            </div>
-          </div>
-        </div>
-        <div className="container">
-          <Row>
-            <Col lg="3" md="6" sm="6">
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="4" xs="5">
-                      <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-globe text-warning" />
-                      </div>
-                    </Col>
-                    <Col md="8" xs="7">
-                      <div className="numbers">
-                        <p className="card-category">Capacity</p>
-                        <CardTitle tag="p">150GB</CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="fas fa-sync-alt" /> Update Now
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col>
-            <Col lg="3" md="6" sm="6">
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="4" xs="5">
-                      <i className="nc-icon nc-money-coins text-success" />
-                    </Col>
-                    <Col md="8" xs="7">
-                      <div className="numbers">
-                        <p className="card-category">Revenue</p>
-                        <CardTitle tag="p">$ 1,345</CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="far fa-calendar" /> Last day
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col>
-            <Col lg="3" md="6" sm="6">
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="4" xs="5">
-                      <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-vector text-danger" />
-                      </div>
-                    </Col>
-                    <Col md="8" xs="7">
-                      <div className="numbers">
-                        <p className="card-category">Errors</p>
-                        <CardTitle tag="p">23</CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="far fa-clock" /> In the last hour
-                  </div>
-                </CardFooter>
-              </Card>
-            </Col>
-            <Col lg="3" md="6" sm="6">
-              <Card className="card-stats">
-                <CardBody>
-                  <Row>
-                    <Col md="4" xs="5">
-                      <div className="icon-big text-center icon-warning">
-                        <i className="nc-icon nc-favourite-28 text-primary" />
-                      </div>
-                    </Col>
-                    <Col md="8" xs="7">
-                      <div className="numbers">
-                        <p className="card-category">Followers</p>
-                        <CardTitle tag="p">+45K</CardTitle>
-                        <p />
-                      </div>
-                    </Col>
-                  </Row>
-                </CardBody>
-                <CardFooter>
-                  <hr />
-                  <div className="stats">
-                    <i className="fas fa-sync-alt" /> Update now
-                  </div>
-                </CardFooter>
-              </Card>
             </Col>
           </Row>
-        </div> */}
+        </div>
+        <div className="container mt-4">
+          {product &&
+            product?.statistics &&
+            product?.statistics?.length > 0 && (
+              <Row>
+                <Col md="6">
+                  <div className="card" style={{ width: "100%" }}>
+                    <div className={`card-header bg-statistics`}>
+                      <h6 className="mt-2 text-center">Statistics</h6>
+                    </div>
+                    <div className="card-body">
+                      <div className="statistics-wrapper">
+                        <ol role="list">
+                          {product?.statistics?.map((statistic: any) => (
+                            <li>
+                              <h6
+                                className="pt-2"
+                                style={{ textTransform: "capitalize" }}
+                              >{`${statistic.name} : ${statistic.value}`}</h6>
+                            </li>
+                          ))}
+                        </ol>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            )}
+        </div>
       </section>
     </>
   );
